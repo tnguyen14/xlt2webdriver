482b55538a84275d82736669ebd6819dcd530570
Tri Nguyen <tri@tridnguyen.com>
2014-09-29 13:04:05 -0400

    parse smoketest

--- index.js
+++ index.js
@@ -6,23 +6,56 @@ var parse = require('./lib/parse');
 var junk = require('junk');
 var mkdirp = require('mkdirp');
 
+// paths
 var scriptsPath = 'vendor/ecom-gui-test/scripts/';
 var outPath = require('./config.json').dist + '/';
 var command = 'A_common/command/';
+var smoketest = 'TESTS/SiteGenesis/PUBLIC/smoketest/';
+var commandPath = scriptsPath + command;
+var commandPathOut = outPath + command;
+var smoketestPath = scriptsPath + smoketest;
+var smoketestPathOut = outPath + smoketest;
 
-fs.exists(outPath + command, function (exists) {
-	if (!exists) {
-		mkdirp.sync(outPath + command);
-	}
+// create out paths
+[commandPathOut, smoketestPathOut].forEach(function (p) {
+	fs.exists(p, function (exists) {
+		if (!exists) {
+			mkdirp.sync(p);
+		}
+	});
 });
 
-fs.readdir(scriptsPath + command, function (err, files) {
+// common commands
+fs.readdir(commandPath, function (err, files) {
 	if (err) throw err;
 	files.filter(junk.not).forEach(function (file) {
-		fs.readFile(scriptsPath + command + file, 'utf8', function (err, res) {
+		fs.readFile(commandPath + file, 'utf8', function (err, res) {
 			if (err) throw err;
 			var fileNameJs = path.basename(file, '.xml') + '.js';
-			fs.writeFileSync(outPath + command + fileNameJs, parse(res));
+			fs.writeFileSync(commandPathOut + fileNameJs, parse(res));
 		});
 	});
+});
+
+function testSuite(ts) {
+	fs.readdir(smoketestPath + ts, function (err, cases) {
+		if (err) throw err;
+		cases.filter(junk.not).forEach(function (tc) {
+			testCase(smoketestPath + ts + '/' + tc);
+		});
+	});
+}
+
+function testCase(tc) {
+	fs.readFile(tc, 'utf8', function (err, res) {
+		if (err) throw err;
+		console.log(tc);
+		console.log(parse(res));
+	});
+}
+
+// smoketest
+fs.readdir(smoketestPath, function (err, suites) {
+	if (err) throw err;
+	suites.forEach(testSuite);
 });
